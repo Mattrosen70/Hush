ec9d68b6b9f0444a0dfc1a6d3028a78cd9438b0f
Mattrosen70 <78610314+Mattrosen70@users.noreply.github.com>
2026-06-23 20:34:25 -0400

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './styles/App.css'
+import HomePage from '@pages/HomePage'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-blue-900">
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
